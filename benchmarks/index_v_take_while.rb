df2b35d3646c0c80b2aa99094e7613a0c4b94aed
David Chelimsky <dchelimsky@gmail.com>
2013-08-24 17:49:56 +0200

    include benchmark output in benchmark file

--- benchmarks/index_v_take_while.rb
+++ benchmarks/index_v_take_while.rb
@@ -1,10 +1,11 @@
 require 'benchmark'
 
-n = 100
+n = 10000
 
 list = 1.upto(10).to_a
 
 Benchmark.benchmark do |bm|
+  puts "take_while"
   3.times do
     bm.report do
       n.times do
@@ -16,6 +17,9 @@ Benchmark.benchmark do |bm|
     end
   end
 
+  puts
+
+  puts "list[0,n]"
   3.times do
     bm.report do
       n.times do
@@ -28,3 +32,16 @@ Benchmark.benchmark do |bm|
     end
   end
 end
+
+__END__
+
+ruby benchmarks/index_v_take_while.rb
+take_while
+   0.020000   0.000000   0.020000 (  0.020005)
+   0.010000   0.000000   0.010000 (  0.015907)
+   0.010000   0.000000   0.010000 (  0.015962)
+
+list[0,n]
+   0.020000   0.000000   0.020000 (  0.023561)
+   0.020000   0.000000   0.020000 (  0.018812)
+   0.030000   0.000000   0.030000 (  0.022389)
